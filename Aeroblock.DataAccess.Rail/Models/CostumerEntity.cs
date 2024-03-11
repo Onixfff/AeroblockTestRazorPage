8e637598981a32d7665377f9ce9e0d38677c6506
Onixfff <danilnikonov01@mail.ru>
2024-03-11 15:05:41 +0200

    Добавил модель для Costumer

---
+++ Aeroblock.DataAccess.Rail/Models/CostumerEntity.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("costumers")]
+    //[Table("costumer")]
+public class CostumerEntity
+{
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name")]
+    public string? Name { get; set; } = string.Empty;
+
+    [Column("adress")]
+    public string? Adress { get; set; } = string.Empty;
+
+    [Column("tel")]
+    public string? Tel { get; set; } = string.Empty;
+
+    [Column("e_mail")]
+    public string? Email { get; set; } = string.Empty;
+
+    [Column("site")]
+    public string? Site { get; set; } = string.Empty;
+
+    [Column("face")]
+    public string? Face { get; set; } = string.Empty;
+
+    [Column("contact_face")]
+    public string? FaceContract { get; set; } = string.Empty;
+}
