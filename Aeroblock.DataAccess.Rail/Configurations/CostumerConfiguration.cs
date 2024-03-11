731b55a98ea376068f71feae43487fbd3ac19070
Onixfff <danilnikonov01@mail.ru>
2024-03-11 15:04:23 +0200

    Создал конфигурацию для Costuner

---
+++ Aeroblock.DataAccess.Rail/Configurations/CostumerConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class CostumerConfiguration : IEntityTypeConfiguration<CostumerEntity>
+{
+    public void Configure(EntityTypeBuilder<CostumerEntity> builder)
+    {
+        builder
+            .HasIndex(x => x.Id)
+            .IsUnique(true);
+        builder
+            .Property(x => x.Id)
+            .HasMaxLength(11)
+            .IsRequired(true);
+
+        builder
+            .Property(x => x.Name)
+            .HasMaxLength(45)
+            .IsRequired(false);
+
+        builder
+            .Property(x => x.Adress)
+            .HasMaxLength(45)
+            .IsRequired(false);
+
+        builder
+            .Property(x => x.Tel)
+            .HasMaxLength(145)
+            .IsRequired(false);
+
+        builder
+            .Property(x => x.Email)
+            .HasMaxLength(145)
+            .IsRequired(false);
+
+        builder
+            .Property(x => x.Site)
+            .HasMaxLength(145)
+            .IsRequired(false);
+
+        builder
+            .Property(x => x.Face)
+            .HasMaxLength(45)
+            .IsRequired(false);
+
+        builder
+            .Property(x => x.FaceContract)
+            .HasMaxLength(45)
+            .IsRequired(false);
+    }
+}
