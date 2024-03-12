0461678341fd71abfb5251ac6cfa5ef5fe6326a3
Onixfff <danilnikonov01@mail.ru>
2024-03-12 10:53:38 +0200

    Создал конфигурацию для NameMaterialEntity

---
+++ Aeroblock.DataAccess.Rail/Configurations/NameMaterialConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Aeroblock.DataAccess.Rail.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class NameMaterialConfiguration : IEntityTypeConfiguration<NameMaterialEntity>
+{
+    public void Configure(EntityTypeBuilder<NameMaterialEntity> builder)
+    {
+        builder
+            .HasIndex(x => x.Id)
+            .IsUnique(true);
+        builder
+            .Property(x => x.Id)
+            .HasMaxLength(11)
+            .IsRequired(true);
+
+        builder
+            .Property(x => x.Name)
+            .HasMaxLength(45)
+            .IsRequired(false);
+
+        builder
+            .Property(x => x.Group)
+            .HasMaxLength(45)
+            .IsRequired(false);
+    }
+}
+
