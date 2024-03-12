03de8bbe1f620afdec70bbb963845032eed99a3d
Onixfff <danilnikonov01@mail.ru>
2024-03-12 11:53:05 +0200

    Создал конфигурацию для SkladEntity

---
+++ Aeroblock.DataAccess.Rail/Configurations/SkladConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Aeroblock.DataAccess.Rail.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class SkladConfiguration : IEntityTypeConfiguration<SkladEntity>
+{
+    public void Configure(EntityTypeBuilder<SkladEntity> builder)
+    {
+        builder
+            .HasIndex(x => x.Id)
+            .IsUnique(true);
+        builder
+            .Property(x => x.Id)
+            .HasMaxLength(11)
+            .IsRequired(true);
+
+        builder
+            .Property(x => x.Name)
+            .HasMaxLength(50)
+            .IsRequired(true);
+        builder
+            .Property(x => x.NameMaterial)
+            .HasMaxLength(128)
+            .IsRequired(true);
+    }
+}
