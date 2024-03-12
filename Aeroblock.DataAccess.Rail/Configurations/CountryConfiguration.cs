f0630f42739b71361ee215024ecdaa9a15a59d16
Onixfff <danilnikonov01@mail.ru>
2024-03-12 09:52:36 +0200

    Добавил конфигурацию к модели

---
+++ Aeroblock.DataAccess.Rail/Configurations/CountryConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Aeroblock.DataAccess.Rail.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class CountryConfiguration : IEntityTypeConfiguration<CountryEntity>
+{
+    public void Configure(EntityTypeBuilder<CountryEntity> builder)
+    {
+        builder
+            .HasIndex(x => x.Id)
+            .IsUnique(true);
+        builder
+            .Property(x => x.Id)
+            .HasMaxLength(11)
+            .IsRequired(true);
+
+        builder
+            .Property(x => x.Name)
+            .HasMaxLength(45)
+            .IsRequired(false);
+    }
+}
