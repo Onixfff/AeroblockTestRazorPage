c01c3f42a9850f83e80fdcee6197aa0d7b7c2a5f
Onixfff <danilnikonov01@mail.ru>
2024-03-12 11:24:17 +0200

    Создал модель PaymentTermEntity

---
+++ Aeroblock.DataAccess.Rail/Models/PaymentTermEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aeroblock.DataAccess.Rail.Models
+{
+    [Table("payment_terms")]
+    //[Table("payment_term")]
+    public class PaymentTermEntity
+    {
+        [Column("id")]
+        public int Id { get; set; } = 0;
+
+        [Column("prepayment")]
+        public int? Prepayment { get; set; } = 0;
+
+        [Column("payment_final")]
+        public int? PaymentFinal { get; set; } = 0;
+
+        [Column("delay")]
+        public int? Delay { get; set; } = 0;
+
+        [Column("name")]
+        public string? Name { get; set; } = string.Empty;
+
+    }
+}
