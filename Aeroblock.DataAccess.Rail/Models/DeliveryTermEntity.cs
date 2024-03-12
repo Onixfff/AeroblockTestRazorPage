76ff43de60ce5d2090dc0f1876cfd47719bf8558
Onixfff <danilnikonov01@mail.ru>
2024-03-12 09:59:28 +0200

    Создал модель

---
+++ Aeroblock.DataAccess.Rail/Models/DeliveryTermEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aeroblock.DataAccess.Rail.Models
+{
+    [Table("delivery_terms")]
+    //[Table("delivery_term")]
+    public class DeliveryTermEntity
+    {
+        [Column("id")]
+        public int Id { get; set; } = 0;
+
+        [Column("name")]
+        public string? Name { get; set; } = string.Empty;
+    }
+}
