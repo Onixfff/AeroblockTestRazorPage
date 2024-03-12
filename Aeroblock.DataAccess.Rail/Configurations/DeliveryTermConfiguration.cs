b3a619190d84c49f52350c34db13eca0d047e01b
Onixfff <danilnikonov01@mail.ru>
2024-03-12 09:59:35 +0200

    Создал конфигурацию

---
+++ Aeroblock.DataAccess.Rail/Configurations/DeliveryTermConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Aeroblock.DataAccess.Rail.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class DeliveryTermConfiguration : IEntityTypeConfiguration<DeliveryTermEntity>
+{
+    public void Configure(EntityTypeBuilder<DeliveryTermEntity> builder)
+    {
+        builder
+            .HasIndex(x => x.Id)
+            .IsUnique(true);
+        builder
+            .Property(x => x.Id)
+            .HasMaxLength(11)
+            .IsRequired(true);
+
+        builder
+            .Property(x => x.Name)
+            .HasMaxLength(245)
+            .IsRequired(false);
+    }
+}
