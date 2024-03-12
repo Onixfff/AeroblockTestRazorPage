a5377a7d4530aab4cec50aad92380a9ac73568fe
Onixfff <danilnikonov01@mail.ru>
2024-03-12 10:53:22 +0200

    Создал модель NameMaterialEntity

---
+++ Aeroblock.DataAccess.Rail/Models/NameMaterialEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aeroblock.DataAccess.Rail.Models
+{
+    [Table("name_material")]
+    //[Table("country")]
+    public class NameMaterialEntity
+    {
+        [Column("id")]
+        public int Id { get; set; } = 0;
+
+        [Column("name")]
+        public string? Name { get; set; } = string.Empty;
+
+        [Column("group")]
+        public string? Group { get; set; } = string.Empty;
+    }
+}
