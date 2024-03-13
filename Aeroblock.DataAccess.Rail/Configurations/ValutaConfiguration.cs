8ac12d9f456760b84da6be5fffd7073bbbd7a356
Onixfff <danilnikonov01@mail.ru>
2024-03-13 22:58:06 +0200

    Добавил ValueEntity модель и конфигурацию

---
+++ Aeroblock.DataAccess.Rail/Configurations/ValutaConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Aeroblock.DataAccess.Rail.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class ValutaConfiguration : IEntityTypeConfiguration<ValutaEntity>
+{
+	public void Configure(EntityTypeBuilder<ValutaEntity> builder)
+	{
+		builder
+			.HasIndex(x => x.Id)
+			.IsUnique(true);
+		builder
+			.Property(x => x.Id)
+			.IsRequired(true);
+
+		builder
+			.Property(x => x.Name)
+			.HasMaxLength(10)
+			.IsRequired(true);
+	}
+}
---
+++ Aeroblock.DataAccess.Rail/Models/ValutaEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aeroblock.DataAccess.Rail.Models
+{
+	[Table("valutas")]
+	//[Table("valuta")]
+	public class ValutaEntity
+	{
+		[Column("id")]
+		public int Id { get; set; } = 0;
+
+		[Column("name")]
+		public string? Name { get; set; } = string.Empty;
+	}
+}
