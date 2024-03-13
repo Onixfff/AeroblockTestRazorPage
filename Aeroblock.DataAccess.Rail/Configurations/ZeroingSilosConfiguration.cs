bdc8347debefc861a7985307816b1c5a86e18037
Onixfff <danilnikonov01@mail.ru>
2024-03-13 22:56:40 +0200

    Создал модель ZeroingSilos, конфигурацию

---
+++ Aeroblock.DataAccess.Rail/Configurations/ZeroingSilosConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Aeroblock.DataAccess.Rail.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class ZeroingSilosConfiguration : IEntityTypeConfiguration<ZeroingSilosEntity>
+{
+	public void Configure(EntityTypeBuilder<ZeroingSilosEntity> builder)
+	{
+		builder
+			.HasIndex(x => x.Id)
+			.IsUnique(true);
+		builder
+			.Property(x => x.Id)
+			.IsRequired(true);
+
+		builder
+			.Property(x => x.Date)
+			.HasColumnType("DATETIME")
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.Weight)
+			.HasColumnType("float")
+			.IsRequired(true);
+
+		builder
+			.Property(x => x.WeightSumIn)
+			.HasColumnType("float")
+			.IsRequired(true);
+
+		builder
+			.Property(x => x.WeightSumOut)
+			.HasColumnType("float")
+			.IsRequired(true);
+
+		builder
+			.Property(x => x.Material)
+			.HasMaxLength(245)
+			.IsRequired(false);
+
+		builder
+			.Property(e => e.SilosId)
+			.IsRequired(true);
+
+		builder.HasOne(x => x.SiloBalance)
+			.WithMany()
+			.HasForeignKey(x => x.SilosId);
+	}
+}
---
+++ Aeroblock.DataAccess.Rail/Models/ZeroingSilosEntity.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aeroblock.DataAccess.Rail.Models
+{
+	[Table("zeroing_siloss")]
+	//[Table("zeroing_silos")]
+	public class ZeroingSilosEntity
+	{
+		[Column("id")]
+		public int Id { get; set; } = 0;
+
+		[Column("date")]
+		public DateTime Date { get; set; } = DateTime.UnixEpoch;
+
+		[Column("weight")]
+		public float Weight { get; set; } = float.MinValue;
+
+		[Column("weight_sum_in")]
+		public float WeightSumIn { get; set;} = float.MinValue;
+
+		[Column("weight_sum_out")]
+		public float WeightSumOut {  get; set;} = float.MinValue;
+
+		[Column("material")]
+		public string Material { get; set; } = string.Empty;
+
+		[Column("id_silos")]
+		public int? SilosId { get; set; }
+
+		public SiloBalanceEntity? SiloBalance { get; set; }
+	}
+}
