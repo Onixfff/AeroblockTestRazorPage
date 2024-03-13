9c42e76547ab656cd9af3197161763448d6f3a05
Onixfff <danilnikonov01@mail.ru>
2024-03-13 22:57:39 +0200

    Добавил VagonSendelr как модель и конфигурацию

---
+++ Aeroblock.DataAccess.Rail/Configurations/VagonSendlerConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Aeroblock.DataAccess.Rail.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Aeroblock.DataAccess.Rail.Configurations
+{
+	public class VagonSendlerConfiguration : IEntityTypeConfiguration<VagonSendlerEntity>
+    {
+        public void Configure(EntityTypeBuilder<VagonSendlerEntity> builder)
+        {
+            builder
+				.HasIndex(x => x.Id)
+				.HasName("id_UNIQUE")
+				.IsUnique(true);
+
+            builder
+				.Property(x => x.Id)
+				.IsRequired(true);
+
+            builder
+				.Property(x => x.Sendler)
+				.HasMaxLength(255)
+				.IsRequired(false);
+		}
+    }
+}
---
+++ Aeroblock.DataAccess.Rail/Models/VagonSendlerEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aeroblock.DataAccess.Rail.Models
+{
+	[Table("vagon_sendlers")]
+	//[Table("vagon_sendler")]
+	public class VagonSendlerEntity
+	{
+		[Column("id")]
+		public int Id { get; set; } = 0;
+
+		[Column("sendler")]
+		public string? Sendler { get; set; } = string.Empty;
+	}
+}
