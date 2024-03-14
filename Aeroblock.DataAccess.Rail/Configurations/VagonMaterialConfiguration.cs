ec25934172b8ac4c66266027539cdc477eecc48e
Onixfff <danilnikonov01@mail.ru>
2024-03-14 09:18:00 +0200

    Создал модель VagonMaterial и конфигурацию

---
+++ Aeroblock.DataAccess.Rail/Configurations/VagonMaterialConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Aeroblock.DataAccess.Rail.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class VagonMaterialConfiguration : IEntityTypeConfiguration<VagonMaterialEntity>
+{
+	public void Configure(EntityTypeBuilder<VagonMaterialEntity> builder)
+	{
+		builder
+			.HasIndex(x => x.Id)
+			.IsUnique(true);
+		builder
+			.Property(x => x.Id)
+			.IsRequired(true);
+
+		builder
+			.Property(x => x.Material)
+			.HasMaxLength(45)
+			.IsRequired(true);
+	}
+}
---
+++ Aeroblock.DataAccess.Rail/Models/VagonMaterialEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aeroblock.DataAccess.Rail.Models
+{
+	[Table("vagon_materials")]
+	//[Table("vagon_material")]
+	public class VagonMaterialEntity
+	{
+		[Column("id")]
+		public int Id { get; set; } = 0;
+
+		[Column("name")]
+		public string? Material { get; set; } = string.Empty;
+	}
+}
