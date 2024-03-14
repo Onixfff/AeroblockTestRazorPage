b88dd28a618b20bd3ca8884d3db59ea9866e0df5
Onixfff <danilnikonov01@mail.ru>
2024-03-14 10:41:13 +0200

    Создал модель и конфигурацию для moveSiloEntity

---
+++ Aeroblock.DataAccess.Rail/Configurations/MoveSiloConfiguration.cs
@@ -0,0 +1,64 @@
+﻿using Aeroblock.DataAccess.Rail.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class MoveSiloConfiguration : IEntityTypeConfiguration<MoveSiloEntity>
+{
+	public void Configure(EntityTypeBuilder<MoveSiloEntity> builder)
+	{
+		builder
+			.HasIndex(x => x.Id)
+			.IsUnique(true);
+		builder
+			.Property(x => x.Id)
+			.IsRequired(true);
+
+		builder
+			.Property(x => x.Timestamp)
+			.HasColumnType("DATETIME")
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.target_silo_id)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.target_silo_material)
+			.HasMaxLength(256)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.target_silo_sendler)
+			.HasMaxLength(256)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.source_silo_id)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.source_silo_material)
+			.HasMaxLength(256)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.source_silo_sendler)
+			.HasMaxLength(256)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.weight)
+			.HasColumnType("float")
+			.IsRequired(false);
+
+		builder
+			.HasOne(x => x.TargetMoveSilo)
+			.WithMany()
+			.HasForeignKey(x => x.target_silo_id);
+
+		builder
+			.HasOne(x => x.SourceMoveSilo)
+			.WithMany()
+			.HasForeignKey(x => x.source_silo_id);
+	}
+}
---
+++ Aeroblock.DataAccess.Rail/Models/MoveSiloEntity.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aeroblock.DataAccess.Rail.Models
+{
+	[Table("move_silos")]
+	//[Table("move_silo")]
+	public class MoveSiloEntity
+	{
+		[Column("id")]
+		public int Id { get; set; } = 0;
+
+		[Column("timestamp")]
+		public DateTime? Timestamp { get; set; } = DateTime.UtcNow;
+
+		[Column("target_silo_id")]
+		public int? target_silo_id { get; set; } = 0;
+
+		[ForeignKey("target_silo_id")]
+		public MoveSiloEntity? TargetMoveSilo { get; set; }
+
+		[Column("target_silo_material")]
+		public string? target_silo_material { get; set; } = string.Empty;
+
+		[Column("target_silo_sendler")]
+		public string? target_silo_sendler { get; set; } = string.Empty;
+
+		[Column("source_silo_id")]
+		public int? source_silo_id { get; set; } = 0;
+
+		[ForeignKey("source_silo_id")]
+		public MoveSiloEntity? SourceMoveSilo { get; set; }
+
+		[Column("source_silo_material")]
+		public string? source_silo_material { get; set; } = string.Empty;
+
+		[Column("source_silo_sendler")]
+		public string? source_silo_sendler { get; set; } = string.Empty;
+
+		[Column("weight")]
+		public float? weight { get; set; } = 0;
+
+
+	}
+}
