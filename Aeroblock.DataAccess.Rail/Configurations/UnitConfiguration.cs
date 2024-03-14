9a4e4afa687f890ee8226823dd2ed2203805f193
Onixfff <danilnikonov01@mail.ru>
2024-03-14 09:19:44 +0200

    Создал модель UnitEntity и конфигурацию

---
+++ Aeroblock.DataAccess.Rail/Configurations/UnitConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Aeroblock.DataAccess.Rail.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class UnitConfiguration : IEntityTypeConfiguration<UnitEntity>
+{
+	public void Configure(EntityTypeBuilder<UnitEntity> builder)
+	{
+		builder
+			.HasIndex(x => x.Id)
+			.IsUnique(true);
+		builder
+			.Property(x => x.Id)
+			.IsRequired(true);
+
+		builder
+			.Property(x => x.Name)
+			.HasMaxLength(10)
+			.IsRequired(true);
+	}
+}
---
+++ Aeroblock.DataAccess.Rail/Models/UnitEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aeroblock.DataAccess.Rail.Models
+{
+	[Table("units")]
+	//[Table("unit")]
+	public class UnitEntity
+	{
+		[Column("id")]
+		public int Id { get; set; } = 0;
+
+		[Column("name")]
+		public string? Name { get; set; } = string.Empty;
+	}
+}
