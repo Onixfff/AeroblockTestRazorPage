e3390b226ca2e2ac2be9c9fab24ebade3d817fb8
Onixfff <danilnikonov01@mail.ru>
2024-03-14 09:19:05 +0200

    Создал модель VagonCostumer и конфигурацию

---
+++ Aeroblock.DataAccess.Rail/Configurations/VagonCostumerConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Aeroblock.DataAccess.Rail.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class VagonCostumerConfiguration : IEntityTypeConfiguration<VagonCostumerEntity>
+{
+	public void Configure(EntityTypeBuilder<VagonCostumerEntity> builder)
+	{
+		builder
+			.HasIndex(x => x.Id)
+			.IsUnique(true);
+		builder
+			.Property(x => x.Id)
+			.IsRequired(true);
+
+		builder
+			.Property(x => x.Costumers)
+			.HasMaxLength(245)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.Adress)
+			.HasMaxLength(245)
+			.IsRequired(false);
+	}
+}
---
+++ Aeroblock.DataAccess.Rail/Models/VagonCostumerEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aeroblock.DataAccess.Rail.Models
+{
+	[Table("vagon_costumerss")]
+	//[Table("vagon_costumers")]
+	public class VagonCostumerEntity
+	{
+		[Column("id")]
+		public int Id { get; set; } = 0;
+
+		[Column("costumers")]
+		public string? Costumers { get; set; } = string.Empty;
+
+		[Column("adress")]
+		public string? Adress { get; set; } = string.Empty;
+	}
+}
