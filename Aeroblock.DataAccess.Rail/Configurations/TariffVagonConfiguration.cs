8b1f2cc0aaac64a189c73dc8e28db3108f71740d
Onixfff <danilnikonov01@mail.ru>
2024-03-14 09:55:22 +0200

    Создал модель TariffVagonEntity и добавил конфигурацию

---
+++ Aeroblock.DataAccess.Rail/Configurations/TariffVagonConfiguration.cs
@@ -0,0 +1,96 @@
+﻿using Aeroblock.DataAccess.Rail.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class TariffVagonConfiguration : IEntityTypeConfiguration<TariffVagonEntity>
+{
+	public void Configure(EntityTypeBuilder<TariffVagonEntity> builder)
+	{
+		builder
+			.HasIndex(x => x.Id)
+			.IsUnique(true);
+		builder
+			.Property(x => x.Id)
+			.IsRequired(true);
+
+		builder
+			.Property(x => x.Station)
+			.HasMaxLength(60)
+			.IsRequired(false);
+		
+		builder
+			.Property(x => x.tarif_full_to_kaliningrad_rf)
+			.HasColumnType("float")
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.nds_tarif_full_to_kaliningrad)
+			.HasMaxLength(1)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.tarif_empty_from_kaliningrad_rf)
+			.HasColumnType("float")
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.nds_tarif_empty_from_kaliningrad_rf)
+			.HasMaxLength(1)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.tarif_full_to_kaliningrad_by)
+			.HasColumnType("float")
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.nds_tarif_full_to_kaliningrad_by)
+			.HasMaxLength(1)
+			.IsRequired(false);
+		
+		builder
+			.Property(x => x.tarif_empty_from_kaliningrad_by)
+			.HasColumnType("float")
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.nds_tarif_empty_from_kaliningrad_by)
+			.HasMaxLength(1)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.tarif_full_to_kaliningrad_lt)
+			.HasColumnType("float")
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.nds_tarif_full_to_kaliningrad_lt)
+			.HasMaxLength(1)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.tarif_empty_from_kaliningrad_lt)
+			.HasColumnType("float")
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.nds_tarif_empty_from_kaliningrad_lt)
+			.HasMaxLength(1)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.rent_vagon)
+			.HasColumnType("float")
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.nds_rent_vagon)
+			.HasMaxLength(1)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.data)
+			.HasColumnType("DATETIME")
+			.IsRequired(false);
+	}
+}
---
+++ Aeroblock.DataAccess.Rail/Models/TariffVagonEntity.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aeroblock.DataAccess.Rail.Models
+{
+	[Table("tariff_vagons")]
+	//[Table("tariff_vagon")]
+	public class TariffVagonEntity
+	{
+		[Column("id")]
+		public int Id { get; set; } = 0;
+
+		[Column("station")]
+		public string? Station { get; set; } = string.Empty;
+
+		[Column("tarif_full_to_kaliningrad_rf")]
+		public float? tarif_full_to_kaliningrad_rf { get; set; } = 0;
+
+		[Column("nds_tarif_full_to_kaliningrad")]
+		public bool? nds_tarif_full_to_kaliningrad { get; set; }
+
+		[Column("tarif_empty_from_kaliningrad_rf")]
+		public float? tarif_empty_from_kaliningrad_rf { get; set; } = 0;
+
+		[Column("nds_tarif_empty_from_kaliningrad_rf")]
+		public bool? nds_tarif_empty_from_kaliningrad_rf { get; set; } = false;
+
+		[Column("tarif_full_to_kaliningrad_by")]
+		public float? tarif_full_to_kaliningrad_by { get; set; } = 0;
+
+		[Column("nds_tarif_full_to_kaliningrad_by")]
+		public bool? nds_tarif_full_to_kaliningrad_by { get; set; } = false;
+
+		[Column("tarif_empty_from_kaliningrad_by")]
+		public float? tarif_empty_from_kaliningrad_by { get; set; } = 0;
+
+		[Column("nds_tarif_empty_from_kaliningrad_by")]
+		public bool? nds_tarif_empty_from_kaliningrad_by { get; set; } = false;
+
+		[Column("tarif_full_to_kaliningrad_lt")]
+		public float tarif_full_to_kaliningrad_lt { get; set; } = 0;
+
+		[Column("nds_tarif_full_to_kaliningrad_lt")]
+		public bool? nds_tarif_full_to_kaliningrad_lt { get; set; } = false;
+
+		[Column("tarif_empty_from_kaliningrad_lt")]
+		public float? tarif_empty_from_kaliningrad_lt { get; set; } = 0;
+
+		[Column("nds_tarif_empty_from_kaliningrad_lt")]
+		public bool? nds_tarif_empty_from_kaliningrad_lt { get; set; } = false;
+
+		[Column("rent_vagon")]
+		public float? rent_vagon { get; set; } = 0;
+
+		[Column("nds_rent_vagon")]
+		public bool? nds_rent_vagon { get; set; } = false;
+
+		[Column("data")]
+		public DateTime data { get; set; } = DateTime.UtcNow;
+	}
+}
