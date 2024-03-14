374b7c12fc80d1b89a4e2bca9ae2f7dccb4a238c
Onixfff <danilnikonov01@mail.ru>
2024-03-14 09:59:06 +0200

    Добавил модель и конфигурацию StationEntity

---
+++ Aeroblock.DataAccess.Rail/Configurations/StationConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Aeroblock.DataAccess.Rail.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class StationConfiguration : IEntityTypeConfiguration<StationEntity>
+{
+	public void Configure(EntityTypeBuilder<StationEntity> builder)
+	{
+		builder
+			.HasIndex(x => x.Id)
+			.IsUnique(true);
+		builder
+			.Property(x => x.Id)
+			.IsRequired(true);
+
+		builder
+			.Property(x => x.Name)
+			.HasMaxLength(45)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.Code)
+			.HasMaxLength(10)
+			.IsRequired(false);
+	}
+}
---
+++ Aeroblock.DataAccess.Rail/Models/StationEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aeroblock.DataAccess.Rail.Models
+{
+	[Table("stations")]
+	//[Table("station")]
+	public class StationEntity
+	{
+		[Column("id")]
+		public int Id { get; set; } = 0;
+
+		[Column("name")]
+		public string? Name { get; set; } = string.Empty;
+
+		[Column("code")]
+		public string? Code { get; set; } = string.Empty;
+	}
+}
