85bbf237bcf7c79e7106305da3b4a1b826ab0a8c
Onixfff <danilnikonov01@mail.ru>
2024-03-14 11:35:42 +0200

    Создал модель и конфигурацию General

---
+++ Aeroblock.DataAccess.Rail/Configurations/GeneralConfiguration.cs
@@ -0,0 +1,133 @@
+﻿using Aeroblock.DataAccess.Rail.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class GeneralConfiguration : IEntityTypeConfiguration<GeneralEntity>
+{
+	public void Configure(EntityTypeBuilder<GeneralEntity> builder)
+	{
+		builder
+			.HasIndex(x => x.Id)
+			.IsUnique(true);
+		builder
+			.Property(x => x.Id)
+			.IsRequired(true);
+
+		builder
+			.Property(x => x.OrderId)
+			.IsRequired(true);
+
+		builder
+			.HasOne(x => x.Order)
+			.WithMany()
+			.HasForeignKey(x => x.OrderId);
+
+		builder
+			.Property(x => x.Group)
+			.HasMaxLength(45)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.NameMaterial)
+			.HasMaxLength(45)
+			.IsRequired(false);
+
+
+		builder
+			.Property(x => x.Country)
+			.HasMaxLength(45)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.Manufakturer)
+			.HasMaxLength(145)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.Costumer)
+			.HasMaxLength(145)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.DeliveryTerm)
+			.HasMaxLength(145)
+			.IsRequired(false);
+
+
+		builder
+			.Property(x => x.Prise)
+			.HasColumnType("float")
+			.IsRequired(false);
+
+
+		builder
+			.Property(x => x.Valuta)
+			.HasMaxLength(10)
+			.IsRequired(false);
+
+
+		builder
+			.Property(x => x.Nds)
+			.HasMaxLength(50)
+			.IsRequired(true);
+
+		builder
+			.Property(x => x.PaymentTerm)
+			.HasMaxLength(145)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.Party)
+			.HasColumnType("float")
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.Tariff)
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.RF)
+			.HasColumnType("float")
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.BY)
+			.HasColumnType("float")
+			.IsRequired(false);
+			
+		builder
+			.Property(x => x.LT)
+			.HasColumnType("float")
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.RentVagon)
+			.HasColumnType("float")
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.SumNotNDS)
+			.HasColumnType("float")
+			.IsRequired(false);
+						
+		builder
+			.Property(x => x.Sum)
+			.HasColumnType("float")
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.SumNDS)
+			.HasColumnType("float")
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.Date)
+			.HasColumnType("DATETIME")
+			.IsRequired(false);
+
+		builder
+			.Property(x => x.Comments)
+			.HasMaxLength(245)
+			.IsRequired(false);
+	}
+}
---
+++ Aeroblock.DataAccess.Rail/Models/GeneralEntity.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aeroblock.DataAccess.Rail.Models
+{
+	[Table("generals")]
+	//[Table("general")]
+	public class GeneralEntity
+	{
+		[Column("id")]
+		public int Id { get; set; } = 0;
+
+		[Column("id_order")]
+		public int? OrderId { get; set; }
+
+		[ForeignKey("OrderId")]
+		public OrderRzdEntity? Order { get; set; }
+
+		[Column("group")]
+		public string? Group { get; set; } = string.Empty;
+
+		[Column("name_material")]
+		public string? NameMaterial { get; set; } = string.Empty;
+
+		[Column("country")]
+		public string? Country { get; set; } = string.Empty;
+
+		[Column("manufakturer")]
+		public string? Manufakturer { get; set; } = string.Empty;
+
+		[Column("costumer")]
+		public string? Costumer { get; set; } = string.Empty;
+
+		[Column("delivery_term")]
+		public string? DeliveryTerm { get; set; } = string.Empty;
+
+		[Column("prise")]
+		public float? Prise { get; set; } = 0;
+
+		[Column("valuta")]
+		public string? Valuta { get; set; } = string.Empty;
+
+		[Column("nds")]
+		public string? Nds { get; set; } = string.Empty;
+
+		[Column("payment_term")]
+		public string? PaymentTerm { get; set; } = string.Empty;
+
+		[Column("party")]
+		public float? Party { get; set; } = 0;
+
+		[Column("tariff")]
+		public int? Tariff { get; set; } = 0;
+
+		[Column("RF")]
+		public float? RF { get; set; } = 0;
+
+		[Column("BY")]
+		public float? BY { get; set; } = 0;
+
+		[Column("LT")]
+		public float? LT { get; set; } = 0;
+
+		[Column("rent_vagon")]
+		public float? RentVagon { get; set; } = 0;
+
+		[Column("sum_not_NDS")]
+		public float? SumNotNDS { get; set; } = 0;
+
+		[Column("sum")]
+		public float? Sum { get; set; } = 0;
+
+		[Column("sum_NDS")]
+		public float? SumNDS { get; set; } = 0;
+
+		[Column("date")]
+		public DateOnly? Date { get; set; } = DateOnly.MinValue;
+
+		[Column("comments")]
+		public string? Comments { get; set; } = string.Empty;
+
+	}
+}
