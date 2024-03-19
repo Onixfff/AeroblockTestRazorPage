c383827c6e3659291885ea7b44ef397f290081e6
Onixfff <danilnikonov01@mail.ru>
2024-03-19 12:02:05 +0200

    Добавил скрипт который не даёт изменить значения checkbox на странице

---
+++ AeroblockTestRazorPage/wwwroot/js/ReturnBaseValueCheckBox.js
@@ -0,0 +1,20 @@
+﻿// Получаем все элементы input на странице
+var allInputs = document.getElementsByTagName('input');
+
+// Создаем пустой массив для хранения checkbox
+var checkboxes = [];
+
+// Проходим по всем элементам input и проверяем их тип
+for (var i = 0; i < allInputs.length; i++) {
+    if (allInputs[i].type === 'checkbox') {
+        checkboxes.push(allInputs[i]);
+        // Добавляем обработчик события для каждого чекбокса
+        allInputs[i].addEventListener('click', function (event) {
+            // Предотвращаем изменение значения чекбокса
+            event.preventDefault();
+        });
+    }
+}
+
+// Теперь массив checkboxes содержит все элементы checkbox на странице
+console.log(checkboxes);
