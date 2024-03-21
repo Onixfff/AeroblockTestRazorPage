d39d7a0927a42e5f4ab927c81a8da502e7556c2f
Onixfff <danilnikonov01@mail.ru>
2024-03-21 14:13:15 +0200

    Изменил интерфейс

---
+++ Aeroblock.DataAccess.Rail/Interface/IMoveSilo.cs
@@ -0,0 +1,9 @@
+﻿using Aeroblock.DataAccess.Rail.Models;
+
+namespace Aeroblock.DataAccess.Rail.Interface
+{
+    internal interface IMoveSilo
+    {
+        public Task<List<SiloBalanceEntity>> GetAsync();
+    }
+}
