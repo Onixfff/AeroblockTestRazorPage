df909eca693e9a6ae9a2c9c05016fdb142cac18a
Onixfff <danilnikonov01@mail.ru>
2024-03-21 14:21:24 +0200

    Добавил repository для вывода всей информации по movesiloEntity

---
+++ Aeroblock.DataAccess.Rail/Repositories/MoveSiloRepository.cs
@@ -0,0 +1,21 @@
+﻿using Aeroblock.DataAccess.Rail.Interface;
+using Aeroblock.DataAccess.Rail.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aeroblock.DataAccess.Rail.Repositories
+{
+    public class MoveSiloRepository(RailDbContext dbContext) : IMoveSilo
+    {
+        private readonly RailDbContext _dbContext = dbContext;
+
+        public async Task<List<MoveSiloEntity>> GetAsync()
+        {
+            return await _dbContext.MoveSilos
+                .AsNoTracking()
+                .Include(x => x.TargetMoveSilo)
+                .Include(x => x.SourceMoveSilo)
+                .OrderByDescending(x => x.Timestamp)
+                .ToListAsync();
+        }
+    }
+}
