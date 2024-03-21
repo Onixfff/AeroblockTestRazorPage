693dda36a578a38abe181f309d37475d1395a675
Onixfff <danilnikonov01@mail.ru>
2024-03-21 14:37:21 +0200

    Добавил интерфейс для реализации главного меню

---
+++ AeroblockTestRazorPage/Interface/IGetModalWindow.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AeroblockTestRazorPage.Interface
+{
+    public interface IGetModalWindow
+    {
+        public IActionResult GetModalWindow();
+    }
+}
